52f93d0afae33c92af710ea5e58e8279034630fe
Julia Augusto <augustojulia979@gmail.com>
2025-03-31 10:33:18 -0300

    Add files via upload

---
+++ aulaPython/cadastrto.py
@@ -0,0 +1,23 @@
+print ("..............PREENCHA O FORMULARIO.........")
+
+# Solicitando as informações do usuário
+
+nome = input ("Digite o seu nome: ")
+idade = float (input("Digite sua idade: "))
+sexo = input ("Digite o seu sexo: ")
+rua = input ("Digite a sua rua: ")
+bairro = input ("Digite o seu bairro: ")
+cidade = input("Digite a sua cidade: ")
+
+# Exibindo as informações inseridas
+
+print (".............................................")
+
+print ("O seu nome é ", nome)
+print ("A sua idade é: ", idade)
+print ("O seu sexo é: ", sexo)
+print ("O seu endereço é: Rua", rua, "Bairro", bairro, "Cidade: ", cidade)
+
+
+
+
